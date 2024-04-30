5e5b0b78d24f47ce9789d7a4a4d2942fb379b7df
qtesterD <daniilandreevich17@gmail.com>
2024-04-30 20:12:26 +0300

    first commit2

--- src/test/java/helper/Driver.java
+++ src/test/java/helper/Driver.java
@@ -2,20 +2,31 @@ package helper;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class Driver {
     public static WebDriver driver;
+    private static ChromeDriverService service;
 
     public static WebDriver initWebDriver() {
+        service = new ChromeDriverService.Builder().usingAnyFreePort().build();
+        try {
+            service.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        driver = new RemoteWebDriver(service.getUrl(), new ChromeOptions());
         ChromeOptions options = new ChromeOptions();
         //options.setBinary("C:\\Users\\worker\\AppData\\Local\\Chromium\\Application\\chrome.exe");
         options.addArguments("--incognito");
 
-        driver = new ChromeDriver(options);
+        // driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
 
@@ -38,6 +49,13 @@ public class Driver {
         driver.get("path");
 
         return driver;
+    }
+
+    @AfterClass
+    public static void stopService() {
+
+        service.stop();
+
     }
 
 
